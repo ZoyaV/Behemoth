c93a121727713f68c2aa225dc5e1c88a700fa49a
Zoya))) <zoya.v@ya.ru>
2021-06-07 15:16:39 +0300

    add request with parametr

--- server/server.py
+++ server/server.py
@@ -4,7 +4,8 @@ from typing import Optional, List
 import numpy as np
 import datetime
 
-from flask import Flask, request, jsonify, Request
+from flask import Flask, request, jsonify, request
+
 from werkzeug.datastructures import FileStorage
 from werkzeug.exceptions import BadRequest
 
@@ -25,12 +26,10 @@ def random_interests():
     global hobbies
     sets = []
     count = []
-
     for hobby in hobbies:
         sets.append(hobby)
         count.append(random.randint(100,200))
     happy_dict = dict(zip(sets, count))
-
     for i in range(random.randint(10, 20)):
         new_arr = np.random.permutation(hobbies)[:random.randint(2,4)].tolist()
         value_arr = [happy_dict[arg] for arg in new_arr]
@@ -38,33 +37,50 @@ def random_interests():
         max_avalible_val = max(value_arr)
         sets.append(new_arr)
         count.append(random.randint(min_avalible_val, max_avalible_val))
-
     result = []
     for i,set in enumerate(sets):
         result.append({'sets':set, 'size':count[i]})
     return result
 
+def random_sex():
+    male = random.randint(100, 200)
+    female = random.randint(100, 200)
+    return male, female
+
+def random_ages():
+    age18 = random.randint(100, 200)
+    age18_26 = random.randint(100, 200)
+    age26_40 = random.randint(100, 200)
+    age40_60 = random.randint(100, 200)
+    age60 = random.randint(100, 200)
+
+    return age18,age18_26,age26_40,age40_60,age60
 
 
-
-@app.route("/sex", methods=['GET'])
+@app.route("/sex", methods=['GET','POST'])
 def sex():
     global male, female
     if datetime.datetime.now().minute %5 == 0:
-        male = random.randint(100,200)
-        female = random.randint(100, 200)
+        if request.args['complex'] == '0':
+            male, female = random_sex()
+        if request.args['complex'] == '1':
+            male, female = random_sex()
+        if request.args['complex'] == '2':
+            male, female = random_sex()
+
     return jsonify({ "male": male,  "female": female})
 
 
-@app.route("/ages", methods=['GET'])
+@app.route("/ages", methods=['GET', 'POST'])
 def ages():
     global age18, age18_26, age26_40, age40_60, age60
     if datetime.datetime.now().minute %5 == 0:
-        age18 = random.randint(100,200)
-        age18_26 = random.randint(100, 200)
-        age26_40 = random.randint(100, 200)
-        age40_60 = random.randint(100, 200)
-        age60 = random.randint(100, 200)
+        if request.args['complex'] == '0':
+            ge18,age18_26,age26_40,age40_60,age60 = random_ages()
+        if request.args['complex'] == '1':
+            ge18,age18_26,age26_40,age40_60,age60 = random_ages()
+        if request.args['complex'] == '2':
+            ge18,age18_26,age26_40,age40_60,age60 = random_ages()
     return jsonify({ "age18": age18,
                      "age18_26": age18_26,
                      "age26_40": age26_40,
@@ -73,11 +89,16 @@ def ages():
                      })
 
 interests_chart = random_interests()
-@app.route("/interests", methods=['GET'])
+@app.route("/interests", methods=['GET','POST'])
 def interests():
     global interests_chart
     if datetime.datetime.now().minute %5 == 0:
-        interests_chart = random_interests()
+        if request.args['complex'] == '0':
+            interests_chart = random_interests()
+        if request.args['complex'] == '1':
+            interests_chart = random_interests()
+        if request.args['complex'] == '2':
+            interests_chart = random_interests()
     return jsonify( {'interst':interests_chart})
 
 
